2028786711b1b7f2888863ae089f4e4042f379ed
gustavo0xf <gals2@cin.ufpe.br>
2026-06-26 11:45:37 -0300

    binary search tree

--- trees/binaryTree/search/main.c
+++ trees/binaryTree/search/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct node {
+    int data;
+    struct node *right;
+    struct node *left;
+} Node;
+
+typedef struct {
+
+} BinarySerchTree;
+
 int main(void) {
 
     return 0;
